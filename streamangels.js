efa4ed694b949c2bb8a6f8724b85c510cbd37487
Gaston <62017364+Gaston-HK@users.noreply.github.com>
2024-03-15 03:04:48 -0300

    0.3

--- streamangels.js
+++ streamangels.js
@@ -1,141 +1,140 @@
-/**
- * 0.2
- * - Allow the client to modify "eurValue" ✔
- * - Allow the client to modify "mValue" ✔
- * - Allow the client to modify "urlBg" ✔
- * - Support for indicating the currency type (experimental) ☢
- * 
- *  * 0.2.1
- * - Auto Reload
- *
- * 0.3
- * To-Do:
- * - Help pop-ups (no alerts)
- *
- * 0.3.1
- * - Add a button to clear "localStorage"
- * - Text corrections for better clarity
- */
+// Extension Version
+const Version = "0.3 [ALPHA]";
 
-// "";
-
+// Tiempo de inicio para medir el rendimiento
 const startTime = performance.now();
-const currencyFormat = ["USD", "ARS"];
-const currencyType = parseInt(localStorage.getItem("typeCurrency") || -1);
+
+// Configuración de recarga automática y fecha actual
 const autoreload = parseInt(localStorage.getItem("autoreload") || 0);
 const currentDate = new Date();
-const Version = "0.2.1 [ALPHA]";
-const eurValue = get_euro_value();
+
+// Valor de un minuto y URL de fondo
 const mValue = parseFloat(localStorage.getItem("mValue") || 0.004);
 const urlBg = localStorage.getItem("urlBg") || "";
+const WithdrawalVal = parseFloat(localStorage.getItem("WithdrawalVal") || 80.0);
+
+// Elemento de referencia donde se insertará la extensión
 const divReference = document.getElementById("alimetro");
+
+// Creación del elemento de la extensión
 const extensionElement = document.createElement("div");
 extensionElement.id = "HKExtension";
 extensionElement.innerHTML = getFormattedDiv();
 extensionElement.classList.add("row", "mt-4", "p-3", "box-4");
+
+// Configuración de fondo si está disponible
 if (urlBg != "") {
   extensionElement.style.background = `url('${urlBg}') no-repeat center`;
   extensionElement.style.backgroundSize = "cover";
 }
-divReference.parentNode.insertBefore(
-  extensionElement,
-  divReference.nextSibling
-);
 
+// Insertar la extensión en el documento
+divReference.parentNode.insertBefore(extensionElement, divReference.nextSibling);
+
+// Creación del elemento de configuración de la extensión
 const newSettings = document.createElement("div");
 newSettings.id = "HKSettings";
 newSettings.innerHTML = get_formateset();
 newSettings.classList.add("row", "mt-4", "p-3", "box-4");
-extensionElement.parentNode.insertBefore(
-  newSettings,
-  extensionElement.nextSibling
-);
+
+// Insertar la configuración en el documento, después de la extensión
+extensionElement.parentNode.insertBefore(newSettings, extensionElement.nextSibling);
 
 var minutesAdded = 0;
 
 function getFormattedDiv() {
-  const lastupdate = currentDate.toString();
-  const balance = parseFloat(getSavedValue());
-  const toAdd = parseFloat(getMonthlyValue());
-  const total = (balance + toAdd).toFixed(2);
-  const formattedDate = formatdate(lastupdate);
-  const lastupdatevalid = localStorage.getItem("datevalid") || "0";
-  const lastminutevalid =
-    parseInt(localStorage.getItem("lastminutevalid")) || 0;
-  const formattedDateValid = formatdate(lastupdatevalid);
-  const totalDay = get_total_day() + minutesAdded;
-  save_total_day(totalDay);
+    // Obtener la fecha actual en formato de cadena de texto
+    const lastupdate = currentDate.toString();
+  
+    // Obtener el saldo almacenado, el valor mensual y calcular el total
+    const balance = parseFloat(getSavedValue());
+    const toAdd = parseFloat(getMonthlyValue());
+    const total = (balance + toAdd).toFixed(2);
+  
+    // Formatear la fecha de la última actualización
+    const formattedDate = formatdate(lastupdate);
+  
+    // Obtener la validez de la última actualización desde el almacenamiento local
+    const lastupdatevalid = localStorage.getItem("datevalid") || "0";
+    const lastminutevalid = parseInt(localStorage.getItem("lastminutevalid")) || 0;
+  
+    // Formatear la fecha de la última actualización válida
+    const formattedDateValid = formatdate(lastupdatevalid);
+  
+    // Obtener el total de minutos del día y actualizarlo
+    const totalDay = get_total_day() + minutesAdded;
+    save_total_day(totalDay);
 
-  const textlast = `<hr>
-    <h6 class="text-decoration-underline text-center">LAST GEM COLLECTED</h6>
-    <li class="mb-2">
-      <i class="fa fa-arrow-right" aria-hidden="true"></i> Last Minutes (valid): ${lastminutevalid}
-    </li> 
-    <li class="mb-2">
-      <i class="fa fa-arrow-right" aria-hidden="true"></i> Last update (valid): ${formattedDateValid}
-    </li>`;
-
-  return `<div class="col-lg-12">
-    <div class="col-12">
-      <span class="element-4 text-center"><i class="fa fa-info-circle" aria-hidden="true"></i>
-       HK Extension</span>
-      <span class="elementor-3"></span>
-    </div>
-    <ul class="list-unstyled">
-      <h6 class="text-decoration-underline text-center">TOTAL MONEY FOR THE DAY</h6>
+    // Tiempo visto
+    const balanceHours = Math.floor(toAdd / 60); // Obtener las horas completas
+    const remainingMinutes = toAdd % 60; // Obtener los minutos restantes
+        
+    // Crear el texto para mostrar la última información válida si existe
+    const textlast = `<hr>
+      <h6 class="text-decoration-underline text-center">LAST GEM COLLECTED</h6>
       <li class="mb-2">
-        <i class="fa fa-arrow-right" aria-hidden="true"></i> Day Balance: ${toAdd.toFixed(
-          2
-        )} <i class="fa fa-eur" aria-hidden="true"></i>
-      </li>
-      <li class="mb-2" ${currencyType === -1 ? 'style="display: none"' : ""}>
-        <i class="fa fa-arrow-right" aria-hidden="true"></i> Day Balance: ${parseFloat(
-          toAdd * eurValue
-        ).toFixed(2)} ${currencyFormat[Math.max(0, currencyType)]}
-      </li>
-      <hr>
-      <h6 class="text-decoration-underline text-center">TOTAL MONEY</h6>
+        <i class="fa fa-arrow-right" aria-hidden="true"></i> Last Minutes (valid): ${lastminutevalid}
+      </li> 
       <li class="mb-2">
-        <i class="fa fa-arrow-right" aria-hidden="true"></i> Total Balance: ${total} <i class="fa fa-eur" aria-hidden="true"></i>
-      </li>
-      <li class="mb-2" ${currencyType === -1 ? 'style="display: none"' : ""}>
-        <i class="fa fa-arrow-right" aria-hidden="true"></i> Total Balance: ${parseFloat(
-          total * eurValue
-        ).toFixed(2)} ${currencyFormat[Math.max(0, currencyType)]}
-      </li>
-      <hr>
-      <h6 class="text-decoration-underline text-center">VALUES</h6>
-      <li class="mb-2" ${currencyType === -1 ? 'style="display: none"' : ""}>
-        <i class="fa fa-arrow-right" aria-hidden="true"></i> Value of EURO: ${eurValue} ${
-    currencyFormat[Math.max(0, currencyType)]
+        <i class="fa fa-arrow-right" aria-hidden="true"></i> Last update (valid): ${formattedDateValid}
+      </li>`;
+  
+    // Construir y devolver la estructura HTML con comentarios
+    return `
+      <div class="col-lg-12">
+        <!-- Información del encabezado -->
+        <div class="col-12">
+          <span class="element-4 text-center"><i class="fa fa-info-circle" aria-hidden="true"></i>
+           HK Extension</span>
+          <span class="elementor-3"></span>
+        </div>
+  
+        <!-- Lista de datos -->
+        <ul class="list-unstyled">
+          <!-- Balance del día -->
+          <h6 class="text-decoration-underline text-center">TOTAL MONEY FOR THE DAY</h6>
+          <li class="mb-2">
+            <i class="fa fa-arrow-right" aria-hidden="true"></i> Day Balance: ${toAdd.toFixed(2)} <i class="fa fa-eur" aria-hidden="true"></i>
+          </li>
+          <hr>
+  
+          <!-- Balance total -->
+          <h6 class="text-decoration-underline text-center">TOTAL MONEY</h6>
+          <li class="mb-2">
+            <i class="fa fa-arrow-right" aria-hidden="true"></i> Total Balance: ${total} <i class="fa fa-eur" aria-hidden="true"></i> | (${(total * 100) / WithdrawalVal}%)
+          </li>
+          <div style="width: 100%; display: block; max-width: 100%; height: 32px; background-color: #FFE500;" class="p-0 mt-3 rounded">
+          <div id='progressbar' class="progress p-0 rounded" style="width: ${Math.max(0, (total * 100) / WithdrawalVal)}%; height: 32px; background-color: #7D2EC5;">
+          </div>
+          </div>
+          <hr class="mt-3">
+  
+          <!-- Información de recarga -->
+          <h6 class="text-decoration-underline text-center">RELOAD INFORMATION</h6>
+          <li class="mb-2">
+            <i class="fa fa-arrow-right" aria-hidden="true"></i> Added minutes: ${minutesAdded}
+          </li>
+          <li class="mb-2">
+            <i class="fa fa-arrow-right" aria-hidden="true"></i> Last Refresh: ${formattedDate}
+          </li>
+          ${lastminutevalid > 0 ? textlast : ""}
+          <hr>
+  
+          <!-- Minutos del día -->
+          <h6 class="text-decoration-underline text-center">DISPLAYED TIME</h6>
+          <li class="mb-2">
+            <i class="fa fa-arrow-right me-1" aria-hidden="true"></i>${balanceHours} hour${balanceHours !== 1 ? 's' : ''}, ${remainingMinutes} minute${remainingMinutes !== 1 ? 's' : ''}
+          </li>
+          <hr>
+  
+          <!-- Versión y tiempo de ejecución -->
+          <li class="mb-2 text-center text-warning">
+            <span>Version: ${Version} | ${(performance.now() - startTime).toFixed(4)} ms</span>
+          </li>
+        </ul>
+      </div>`;
   }
-      </li>
-      <li class="mb-2">
-        <i class="fa fa-arrow-right" aria-hidden="true"></i> Value of Minute: ${mValue}
-      </li>
-      <hr>
-      <h6 class="text-decoration-underline text-center">RELOAD INFORMATION</h6>
-      <li class="mb-2">
-        <i class="fa fa-arrow-right" aria-hidden="true"></i> Added minutes: ${minutesAdded}
-      </li>
-      <li class="mb-2">
-        <i class="fa fa-arrow-right" aria-hidden="true"></i> Last Refresh: ${formattedDate}
-      </li>
-      ${lastminutevalid > 0 ? textlast : ""}
-      <hr>
-      <h6 class="text-decoration-underline text-center">MINUTES DAY</h6>
-      <li class="mb-2">
-        <i class="fa fa-arrow-right" aria-hidden="true"></i> Total minutes of the day: ${totalDay}
-      </li>
-      <hr>
-      <li class="mb-2 text-center text-warning">
-        <span>Version: ${Version} | ${(performance.now() - startTime).toFixed(
-    4
-  )} ms</span>
-      </li>
-    </ul>
-  </div>`;
-}
+  
 
 function getMonthlyValue() {
   const elementMinutes = document.getElementById("tspan753");
@@ -229,18 +228,8 @@ function get_formateset() {
     </li>
     <li class="mb-2">
       <div class="mb-3">
-        <label for="typeCurrency" class="form-label">Type Currency</label>
-        <select class="form-select" id="typeCurrency">
-          <option value="-1" ${
-            currencyType === -1 ? "selected" : ""
-          }>NINGUNA</option>
-          <option value="0" ${
-            currencyType === 0 ? "selected" : ""
-          }>USD (Dólar estadounidense)</option>
-          <option value="1" ${
-            currencyType === 1 ? "selected" : ""
-          }>ARS (Peso argentino)</option>
-        </select>
+        <label for="WithdrawalVal" class="form-label">Minimum Withdrawal</label>
+        <input type="text" class="form-control" id="WithdrawalVal" value="${WithdrawalVal}"/>
       </div>
     </li>
     <li class="mb-2">
@@ -262,35 +251,22 @@ function get_formateset() {
 </div>`;
 }
 
-function get_euro_value() {
-  if (currencyType === -1)
-    // None
-    return 1.0;
-  else if (currencyType === 0)
-    // USD
-    return 1.09;
-  else if (currencyType === 1)
-    // ARS
-    return 994.8467;
-
-  return 1.0;
-}
-
 const hkupdate = document.getElementById("hkupdate");
 hkupdate.addEventListener("click", updatesettings);
 
 function updatesettings() {
-  const typeCurrencyForm = document.getElementById("typeCurrency");
-  localStorage.setItem("typeCurrency", typeCurrencyForm.value);
-
   const mValueForm = document.getElementById("mValue");
   localStorage.setItem("mValue", mValueForm.value);
 
   const urlBgForm = document.getElementById("urlBg");
   localStorage.setItem("urlBg", urlBgForm.value);
 
+  const WithdrawalVal = document.getElementById("WithdrawalVal");
+  localStorage.setItem("WithdrawalVal", WithdrawalVal.value);
+
   location.reload();
 }
+
 // EXPERIMENTAL AUTO RELOAD
 const hkautoreloadbtn = document.getElementById("hkautoreload");
 hkautoreloadbtn.addEventListener("click", hkautoreload);
@@ -298,7 +274,6 @@ hkautoreloadbtn.addEventListener("click", hkautoreload);
 function hkautoreload()
 {
   localStorage.setItem("autoreload", autoreload === 0 ? 1 : 0);
-
   location.reload();
 }
 
